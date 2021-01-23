659af3796baf5bd1e9a80434f7c2bfef6bac9dee
Filip Cornell <filipcornell@Filips-MBP.lan>
2021-01-23 21:33:53 +0100

    Dded some work

--- src/run.py
+++ src/run.py
@@ -3,14 +3,27 @@ import torch.nn as nn
 from typing import Callable
 
 
-def run_one_epoch(
-    model: nn.Module, data_loader: torch.utils.data.DataLoader, loss_function: Callable
+def run_one_vae_epoch(
+    model, data_loader: torch.utils.data.DataLoader, loss_function: Callable, is_train : bool, optimizer
 ):
-    pass
+
+    if is_training is True:
+        optimizer.zero_grad()
+
+    for batch, labels in data_loader:
+        (mu, logvar), recons = model(batch)
 
 
-def train():
-    pass
+
+
+
+
+def train_vae(model, n_epochs : int, train_loader, val_loader, optimizer):
+    if use_cuda is True:
+        assert torch.cuda.is_available()
+        model.cuda()
+    for epoch in range(n_epochs):
+
 
 
 def eval():
--- src/vae.py
+++ src/vae.py
@@ -4,20 +4,44 @@ from typing import Tuple
 
 
 # Highly inspired by https://github.com/SashaMalysheva/Pytorch-VAE/
+# And https://arxiv.org/abs/1606.05908
+# Taking the same architecture as
 class VAE(nn.Module):
-    def __init__(self):
+    def __init__(self, n_channels, n_kernels, latent_size):
         super().__init__()
+        self.n_channels = n_channels
+        self.n_kernels = n_kernels
+        self.latent_size = latent_size
 
-        self.encoder = self.create_encoder()
 
-    def create_encoder(self):
-        pass
+    def create_encoder(self, encoder_output_size):
+        self.encoder = nn.Sequential(
+            self._conv(n_channels, n_kernels // 4),
+            self._conv(n_channels, n_kernels // 2),
+            self._conv(n_channels, n_kernels),
+        )
+        # A layer to arrive at the mus
+        self.q_mu = nn.Linear(, latent_size)
+        self.q_vars = nn.Linear(, latent_size)
+
 
     def forward(self, x) -> Tuple[tuple, torch.Tensor]:
+        # First, derive a latent representation
         latent = self.encoder(x)
-        # get the mus
-        mu, sigma = self.q()
+        # Then, from the latent variables' PDF of Q(z), sample the means and the vars
+        mus, sigmas = self.Q()
+
+
+
+
         return (mu, sigma), latent
 
-    def q(self):
+    def Q(self, mu, var):
+        # Q is the latent variables' PDF
+
         pass
+
+    def _conv(self, n_channels, n_kernels):
+        return nn.Sequential(
+
+        )
